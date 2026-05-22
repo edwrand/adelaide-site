dbc8a2b669638ddd7d6165640bbac98f0047c60c
edwrand <ebbyrandall@yahoo.com>
2026-05-22 18:33:26 -0400

    custom domain setup:

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/adelaide-site",
-  env: { NEXT_PUBLIC_BASE_PATH: "/adelaide-site" },
   images: { unoptimized: true },
 };
 
